64888cc60cfd249455df588ed1b92f3aebdcd127
Amit Or <128776171+selwordamit@users.noreply.github.com>
2026-08-20 20:19:03 +0300

    Add validation library project files

---
+++ src/main/java/il/ac/hit/validation/BasicUser.java
@@ -0,0 +1,20 @@
+package il.ac.hit.validation;
+
+/**
+ * Represents a user with the basic membership type.
+ */
+public class BasicUser extends User {
+    /**
+     * Creates a basic user.
+     *
+     * @param username the user's sign-in name
+     * @param email the user's email address
+     * @param password the user's password
+     * @param age the user's age in years
+     */
+    public BasicUser(String username, String email, String password, int age) {
+        super(username, email, password, age);
+    }
+}
+
+
