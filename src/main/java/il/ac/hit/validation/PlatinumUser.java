944ff29997774738073c7b4b433b679a8d1d0f45
Amit Or <128776171+selwordamit@users.noreply.github.com>
2026-08-20 20:19:06 +0300

    Add validation library project files

---
+++ src/main/java/il/ac/hit/validation/PlatinumUser.java
@@ -0,0 +1,20 @@
+package il.ac.hit.validation;
+
+/**
+ * Represents a user with the platinum membership type.
+ */
+public class PlatinumUser extends User {
+    /**
+     * Creates a platinum user.
+     *
+     * @param username the user's sign-in name
+     * @param email the user's email address
+     * @param password the user's password
+     * @param age the user's age in years
+     */
+    public PlatinumUser(String username, String email, String password, int age) {
+        super(username, email, password, age);
+    }
+}
+
+
